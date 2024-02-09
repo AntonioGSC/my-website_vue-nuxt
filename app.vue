424469bc7eadd7a6e75f51ba8eb2c6297d8c880f
Antonio Carvalho <antoniogabrielagsc@gmail.com>
2024-02-08 22:51:03 -0300

    feat: finish project

--- app.vue
+++ app.vue
@@ -1,5 +1,30 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+    <ProfileInfo
+        :photo="photo"
+        title="Antonio Carvalho"
+        subtitle="@antoniogsc"
+        headline="Front-end Developer"
+    />
+   
+    <div class="max-w-xs mx-auto">
+        <ButtonAction 
+            text="Website"
+            link="https://www.antoniocarvalho.dev"
+            icon="💬"
+        />
+        <ButtonAction 
+            text="Linkedin"
+            link="https://www.linkedin.com/in/antoniogscarvalho/"
+            icon="💼"
+        />
+        <ButtonAction 
+            text="Github"
+            link="https://github.com/AntonioGSC"
+            icon="🛠"
+        />
+    </div>
 </template>
+
+<script setup>
+    import photo from "./assets/profile.png";
+</script>
